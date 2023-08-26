6ed9f2a2497e183319712a79832d24a9247e6aad
Aldoordl <geppai29@gmail.com>
2023-08-26 15:27:28 +0700

    Update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// JavaScript
+function setTitle(element) {
+    const text = element.innerText;
+  
+    document.title = `${text} - SMKN 1 PURWOSARI`;
+  }
+
+function resetTitle() {
+  document.title = "Welcome to SMKN 1 Purwosari";
+}
+document.querySelector(".btn a").addEventListener("click", resetTitle);
+ 
+var typed = new Typed(".typing",{
+  strings:["","Aldoordl! ","People! ","Teacher! ","Student! "],
+  typeSpeed:100,
+  BakSpeed:60,
+  loop:true
+})
