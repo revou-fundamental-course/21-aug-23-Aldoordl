bfd6b0c4d13ca213b09b21068bf3364db807884b
Aldoordl <geppai29@gmail.com>
2023-08-30 19:21:31 +0700

    Update

--- js/script.js
+++ js/script.js
@@ -9,8 +9,33 @@ function resetTitle() {
 }
 document.querySelector(".btn a").addEventListener("click", resetTitle);
  
+// Cek apakah pengguna telah memberikan nama sebelumnya di localStorage
+const storedName = localStorage.getItem('user');
+if (storedName) {
+    document.getElementById("name").innerHTML = storedName;
+} else {
+    // Jalankan replaceName jika nama belum ada
+    replaceName();
+}
+
+// Fungsi untuk mengganti nama
+function replaceName() {
+    let name = prompt("What's your name?", "");
+    if (name !== null && name.trim() !== "") {
+        document.getElementById("name").innerHTML = name;
+        // Simpan nama di localStorage agar bisa digunakan pada kunjungan berikutnya
+        localStorage.setItem('user', name);
+    }
+}
+
+// Tambahkan event listener ke tombol
+document.getElementById('tombol').addEventListener("click", function() {
+    replaceName();
+});
+
+// typed.js
 var typed = new Typed(".typing",{
-  strings:["","Aldoordl! ","People! ","Teacher! ","Student! "],
+  strings:["People! ","Teacher! ","Student! "],
   typeSpeed:100,
   BakSpeed:60,
   loop:true
